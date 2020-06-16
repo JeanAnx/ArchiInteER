7bc26a990ea0cdfb06b1f4bc3416b2e63d47da11
JeanAnx <jean.annaix@gmail.com>
2020-06-16 18:58:09 +0200

    voiture balaiiiii

--- js/images.js
+++ js/images.js
@@ -1,13 +1,24 @@
 
-  var imgs = document.getElementsByClassName('loading-image');
-  for (let i = 0; i < imgs.length; i++) {
-    imgs[i].addEventListener('load', function() {
-      setTimeout(function() {
-        var parent = imgs[i].parentNode;
-        var grandParent = parent.parentNode;
-        grandParent.classList.remove('spinner');
-      }, 500);
-    });
-  }
+var imgs = document.getElementsByClassName('loading-image');
+for (let i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener('load', function() {
+    setTimeout(function() {
+      var parent = imgs[i].parentNode;
+      var grandParent = parent.parentNode;
+      grandParent.classList.remove('spinner');
+    }, 500);
+  });
+}
 
+function voitureBalai() {
+    for (let i = 0; i < imgs.length; i++) {
+      var parent = imgs[i].parentNode;
+      var grandParent = parent.parentNode;
+      grandParent.classList.remove('spinner');
+        }
+    };
 
+setTimeout(function() {
+  voitureBalai();
+  console.log('VOITURE BALAIIIII');
+}, 5000);
